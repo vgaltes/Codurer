a55d26e17601c91079710dd84d5f77f212f35614
vigarcia <vigarcia@plainconcepts.com>
2014-02-10 23:50:04 +0100

    WhenAddingAnExistingUser_TheUserIsNotAddedToCollection -> red

--- Codurer/Codurer.Test/InMemoryUserServicePostingTests.cs
+++ Codurer/Codurer.Test/InMemoryUserServicePostingTests.cs
@@ -18,5 +18,18 @@
             userService.Users.Should().HaveCount(1);
             userService.Users.First().Name.Should().Be(userName);
         }
+
+        [TestMethod]
+        public void WhenAddingAnExistingUser_TheUserIsNotAddedToCollection()
+        {
+            var userService = new InMemoryUserService();
+            var userName = "Alice";
+
+            userService.AddUser(userName);
+            userService.AddUser(userName);
+
+            userService.Users.Should().HaveCount(1);
+            userService.Users.First().Name.Should().Be(userName);
+        }
     }
 }
