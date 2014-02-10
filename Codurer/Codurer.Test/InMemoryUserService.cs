f486f9173c9e16a97dc74cac53dbada2480b1c7c
vigarcia <vigarcia@plainconcepts.com>
2014-02-10 23:43:32 +0100

    WhenAddingAUser_ANewUserIsAddedToCollection -> red

---
+++ Codurer/Codurer.Test/InMemoryUserService.cs
@@ -0,0 +1,24 @@
+﻿namespace CodurerApp.Test
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class InMemoryUserService : UserService
+    {
+        public IList<User> Users
+        {
+            get;
+            set;
+        }
+
+        public void AddUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Post(string message, string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Codurer/Codurer.Test/InMemoryUserServicePostingTests.cs
@@ -0,0 +1,22 @@
+﻿namespace CodurerApp.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using FluentAssertions;
+    using System.Linq;
+
+    [TestClass]
+    public class InMemoryUserServicePostingTests
+    {
+        [TestMethod]
+        public void WhenAddingAUser_ANewUserIsAddedToCollection()
+        {
+            var userService = new InMemoryUserService();
+            var userName = "Alice";
+
+            userService.AddUser(userName);
+
+            userService.Users.Should().HaveCount(1);
+            userService.Users.First().Name.Should().Be(userName);
+        }
+    }
+}
