9b901b59dc653fbbd19101cf4344bae072c826ef
vigarcia <vigarcia@plainconcepts.com>
2014-02-12 00:31:51 +0100

    GivenAMessagePostedLessThanOneSecondAgo_FormatReturnsTheMessagePlusNow -> red

---
+++ Codurer/Codurer.Test/MessageTests.cs
@@ -0,0 +1,21 @@
+﻿namespace CodurerApp.Test
+{
+    using FluentAssertions;
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MessageTests
+    {
+        [TestMethod]
+        public void GivenAMessagePostedLessThanOneSecondAgo_FormatReturnsTheMessagePlusNow()
+        {
+            var messageText = "message text";
+            var message = new Message(messageText, DateTime.Now);
+
+            var result = message.Format();
+
+            result.Should().Be(messageText + " (now)");
+        }
+    }
+}
