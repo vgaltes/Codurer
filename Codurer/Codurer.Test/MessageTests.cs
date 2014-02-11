f3ab06c428301a04f5658f93da7cf283983eae44
vigarcia <vigarcia@plainconcepts.com>
2014-02-12 00:36:11 +0100

    GivenAMessagePostedMoreThanOneSecondAgoAndLessThanOneMinuteAgo_FormatReturnsTheMessagePlusSeconds -> red

--- Codurer/Codurer.Test/MessageTests.cs
+++ Codurer/Codurer.Test/MessageTests.cs
@@ -17,5 +17,16 @@
 
             result.Should().Be(messageText + " (now)");
         }
+
+        [TestMethod]
+        public void GivenAMessagePostedMoreThanOneSecondAgoAndLessThanOneMinuteAgo_FormatReturnsTheMessagePlusSeconds()
+        {
+            var messageText = "message text";
+            var message = new Message(messageText, DateTime.Now.AddSeconds(-2));
+
+            var result = message.Format();
+
+            result.Should().Be(messageText + " (2 seconds ago)");
+        }
     }
 }
