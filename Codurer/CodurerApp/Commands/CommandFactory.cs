6c3be60704b6a3f3610b3ead91e3e137566234ce
vigarcia <vigarcia@plainconcepts.com>
2014-02-12 23:26:06 +0100

    GetTimelineCommand -> green

--- Codurer/CodurerApp/Commands/CommandFactory.cs
+++ Codurer/CodurerApp/Commands/CommandFactory.cs
@@ -7,6 +7,8 @@
         {
             if (commandLine.Contains("->"))
                 return new PostCommand();
+            else if (commandLine.Split(new string[] { "->" }, StringSplitOptions.RemoveEmptyEntries).Length == 1)
+                return new TimelineCommand();
 
             throw new ArgumentException("The command line contains no valid command.");
         }
