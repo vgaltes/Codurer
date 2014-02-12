228f3458a349397b860ff52ff569d2a535793aa1
vigarcia <vigarcia@plainconcepts.com>
2014-02-13 00:35:50 +0100

    Refactor

--- Codurer/Codurer.Test/CodurerIntegrationTests.cs
+++ Codurer/Codurer.Test/CodurerIntegrationTests.cs
@@ -18,11 +18,11 @@
             var firstAlicePostCommand = CommandFactory.GetCommand("Alice -> First message", codurer);
             firstAlicePostCommand.Execute(now.AddMinutes(-5));
             var secondAlicePostCommand = CommandFactory.GetCommand("Alice -> Second message", codurer);
-            secondAlicePostCommand.Execute(now.AddMinutes(-4));
+            secondAlicePostCommand.Execute(now.AddMinutes(-1));
             var thirdAlicePostCommand = CommandFactory.GetCommand("Alice -> Third message", codurer);
-            thirdAlicePostCommand.Execute(now.AddMinutes(-1));
+            thirdAlicePostCommand.Execute(now.AddSeconds(-30));
             var firstBobPostCommand = CommandFactory.GetCommand("Bob -> First message", codurer);
-            firstBobPostCommand.Execute(now.AddSeconds(-30));
+            firstBobPostCommand.Execute(now.AddMinutes(-4));
             var secondBobPostCommand = CommandFactory.GetCommand("Bob -> Second message", codurer);
             secondBobPostCommand.Execute(now);
 
@@ -33,9 +33,13 @@
             var bobWall = bobWallCommand.Execute();
 
             aliceWall.Items.Should().HaveCount(3);
-            aliceWall.Items.First().Should().Be("Third message (1 minute ago)");
-            aliceWall.Items.Skip(1).Take(1).First().Should().Be("Second message (4 minutes ago)");
+            aliceWall.Items.First().Should().Be("Third message (30 seconds ago)");
+            aliceWall.Items.Skip(1).Take(1).First().Should().Be("Second message (1 minute ago)");
             aliceWall.Items.Skip(2).Take(1).First().Should().Be("First message (5 minutes ago)");
+
+            bobWall.Items.Should().HaveCount(2);
+            bobWall.Items.First().Should().Be("Second message (now)");
+            bobWall.Items.Skip(1).Take(1).First().Should().Be("First message (4 minutes ago)");
         }
     }
 }
