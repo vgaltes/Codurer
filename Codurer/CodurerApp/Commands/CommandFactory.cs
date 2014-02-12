3ecc214ce7893ae92d2e81023bf26f7f4f58f090
vigarcia <vigarcia@plainconcepts.com>
2014-02-12 23:20:54 +0100

    GetPostCommand -> green

--- Codurer/CodurerApp/Commands/CommandFactory.cs
+++ Codurer/CodurerApp/Commands/CommandFactory.cs
@@ -1,10 +1,14 @@
 ﻿namespace CodurerApp.Commands
 {
+    using System;
     public class CommandFactory
     {
         public static Command GetCommand(string commandLine)
         {
-            throw new System.NotImplementedException();
+            if (commandLine.Contains("->"))
+                return new PostCommand();
+
+            throw new ArgumentException("The command line contains no valid command.");
         }
     }
 }
