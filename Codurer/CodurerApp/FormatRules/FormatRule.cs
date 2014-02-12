5b22365d7cbb58c8d2e0cbcf77005fe319e50e60
vigarcia <vigarcia@plainconcepts.com>
2014-02-12 01:39:29 +0100

    Console application.

---
+++ Codurer/CodurerApp/FormatRules/FormatRule.cs
@@ -0,0 +1,11 @@
+﻿namespace CodurerApp.FormatRules
+{
+    using System;
+
+    public interface FormatRule
+    {
+        bool IsSatisfied(DateTime now, DateTime postingTime);
+
+        string Format(string messageText, DateTime now, DateTime postingTime);
+    }
+}
---
+++ Codurer/CodurerApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodurerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var codurer = new Codurer(new InMemoryUserService());
+            var quitString = "!q";
+
+            string line = Console.ReadLine();
+                                   
+            while (line != quitString)
+            {
+                var wall = codurer.Send(line);
+                foreach ( string wallMessage in wall)
+                {
+                    Console.WriteLine(wallMessage);
+                }
+
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
