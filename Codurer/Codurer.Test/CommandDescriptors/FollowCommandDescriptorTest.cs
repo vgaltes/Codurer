69357569f2e99755d36ff2f4091cfcf1b278edb9
vigarcia <vigarcia@plainconcepts.com>
2014-02-14 21:28:53 +0100

    WhenFollowing_EvaluatingTheCommandReturnsTrue -> green

---
+++ Codurer/Codurer.Test/CommandDescriptors/FollowCommandDescriptorTest.cs
@@ -0,0 +1,28 @@
+﻿namespace CodurerApp.Test.CommandDescriptors
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Linq;
+    using CodurerApp.Commands;
+    using FluentAssertions;
+
+    [TestClass]
+    public class FollowCommandDescriptorTest
+    {
+        CommandDescriptor<FollowCommand> followCommandDescriptor = new CommandDescriptor<FollowCommand>(
+                commandLine => commandLine.Contains("follows"),
+                commandLine => commandLine
+                    .Split(new string[] { "follows" }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(parameter => parameter.Trim())
+                    .ToArray<string>());
+
+        string commandLine = "Alice follows Bob";
+
+        [TestMethod]
+        public void WhenFollowing_EvaluatingTheCommandReturnsTrue()
+        {
+            var isFollowCommand = followCommandDescriptor.IsCommand(commandLine);
+            isFollowCommand.Should().BeTrue();
+        }
+    }
+}
