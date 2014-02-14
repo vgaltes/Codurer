72af4f4797248f9fd01a9eb49c8e16b305e3bdd1
vigarcia <vigarcia@plainconcepts.com>
2014-02-14 21:24:12 +0100

    WhenRetrievingTheTimeline_EvaluatingTheCommandReturnsTrue -> green

---
+++ Codurer/Codurer.Test/CommandDescriptors/TimelineCommandDescriptorTest.cs
@@ -0,0 +1,25 @@
+﻿namespace CodurerApp.Test.CommandDescriptors
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Linq;
+    using CodurerApp.Commands;
+    using FluentAssertions;
+
+    [TestClass]
+    public class TimelineCommandDescriptorTest
+    {
+        CommandDescriptor<TimelineCommand> timelineCommandDescriptor = new CommandDescriptor<TimelineCommand>(
+                commandLine => commandLine.Split(new string[]{" "}, StringSplitOptions.RemoveEmptyEntries).Count() == 1,
+                commandLine => new string[] { commandLine });
+
+        string commandLine = "Alice";
+
+        [TestMethod]
+        public void WhenRetrievingTheTimeline_EvaluatingTheCommandReturnsTrue()
+        {
+            var isPostCommand = timelineCommandDescriptor.IsCommand(commandLine);
+            isPostCommand.Should().BeTrue();
+        }
+    }
+}
