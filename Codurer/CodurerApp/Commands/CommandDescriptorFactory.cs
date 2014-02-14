96c129e71aa5e665ad0ff5b13a4bf4463c307cfa
vigarcia <vigarcia@plainconcepts.com>
2014-02-14 22:13:53 +0100

    Refactor

---
+++ Codurer/CodurerApp/Commands/CommandDescriptorFactory.cs
@@ -0,0 +1,45 @@
+﻿namespace CodurerApp.Commands
+{
+    using System;
+    using System.Linq;
+
+    public static class CommandDescriptorFactory
+    {
+        public static CommandDescriptor GetPostCommandDescriptor()
+        {
+            return new CommandDescriptor(typeof(PostCommand),
+                cLine => cLine.Contains("->"),
+                cLine => cLine
+                    .Split(new string[] { "->" }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(parameter => parameter.Trim())
+                    .ToArray<string>());
+        }
+
+        public static CommandDescriptor GetTimelineCommandDescriptor()
+        {
+            return new CommandDescriptor(typeof(TimelineCommand),
+                cLine => cLine.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Count() == 1,
+                cLine => new string[] { cLine });
+        }
+
+        public static CommandDescriptor GetFollowingCommandDescriptor()
+        {
+            return new CommandDescriptor(typeof(FollowCommand),
+                cLine => cLine.Contains("follows"),
+                cLine => cLine
+                    .Split(new string[] { "follows" }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(parameter => parameter.Trim())
+                    .ToArray<string>());
+        }
+
+        public static CommandDescriptor GetWallCommandDescriptor()
+        {
+            return new CommandDescriptor(typeof(WallCommand),
+                cLine => cLine.Contains("wall"),
+                cLine => cLine
+                    .Split(new string[] { "wall" }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(parameter => parameter.Trim())
+                    .ToArray<string>());
+        }
+    }
+}
--- Codurer/CodurerApp/Commands/CommandFactory.cs
+++ Codurer/CodurerApp/Commands/CommandFactory.cs
@@ -8,30 +8,10 @@
     {
         public static Command GetCommand(string commandLine, Codurer codurer)
         {
-            var postCommandDescriptor = new CommandDescriptor(typeof(PostCommand),
-                cLine => cLine.Contains("->"),
-                cLine => cLine
-                    .Split(new string[] { "->" }, StringSplitOptions.RemoveEmptyEntries)
-                    .Select(parameter => parameter.Trim())
-                    .ToArray<string>());
-
-            var timelineCommandDescriptor = new CommandDescriptor(typeof(TimelineCommand),
-                cLine => cLine.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Count() == 1,
-                cLine => new string[] { cLine });
-
-            var followCommandDescriptor = new CommandDescriptor(typeof(FollowCommand),
-                cLine => cLine.Contains("follows"),
-                cLine => cLine
-                    .Split(new string[] { "follows" }, StringSplitOptions.RemoveEmptyEntries)
-                    .Select(parameter => parameter.Trim())
-                    .ToArray<string>());
-
-            var wallCommandDescriptor = new CommandDescriptor(typeof(WallCommand),
-                cLine => cLine.Contains("wall"),
-                cLine => cLine
-                    .Split(new string[] { "wall" }, StringSplitOptions.RemoveEmptyEntries)
-                    .Select(parameter => parameter.Trim())
-                    .ToArray<string>());
+            var postCommandDescriptor = CommandDescriptorFactory.GetPostCommandDescriptor();
+            var timelineCommandDescriptor = CommandDescriptorFactory.GetTimelineCommandDescriptor();
+            var followCommandDescriptor = CommandDescriptorFactory.GetFollowingCommandDescriptor();
+            var wallCommandDescriptor = CommandDescriptorFactory.GetWallCommandDescriptor();
 
             var commandDescriptors = new List<CommandDescriptor>
             {
