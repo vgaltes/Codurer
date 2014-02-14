355b20bf7a7c8b3c2b18d2c0b3dcef5e2b91f6f6
vigarcia <vigarcia@plainconcepts.com>
2014-02-14 20:27:17 +0100

    WhenPostingAMessage_EvaluatingTheCommandReturnsTrue -> red

---
+++ Codurer/Codurer.Test/CommandDescriptors/PostCommandDescriptorTest.cs
@@ -0,0 +1,22 @@
+﻿namespace CodurerApp.Test.CommandDescriptors
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using CodurerApp.Commands;
+    using FluentAssertions;
+
+    [TestClass]
+    public class PostCommandDescriptorTest
+    {
+        [TestMethod]
+        public void WhenPostingAMessage_EvaluatingTheCommandReturnsTrue()
+        {
+            var postCommandDescriptor = new CommandDescriptor<PostCommand>(
+                commandLine => commandLine.Contains("->"),
+                commandLine => commandLine.Split(new string[] { "->" }, StringSplitOptions.RemoveEmptyEntries));
+
+            var isPostCommand = postCommandDescriptor.IsCommand("Alice -> new message");
+            isPostCommand.Should().BeTrue();
+        }
+    }
+}
---
+++ Codurer/CodurerApp/Commands/CommandDescriptor.cs
@@ -0,0 +1,22 @@
+﻿namespace CodurerApp.Commands
+{
+    using System;
+
+    public class CommandDescriptor<T>
+    {
+        Func<string, bool> conditionEvaluation;
+        Func<string, string[]> parametersExtraction;
+        
+        public CommandDescriptor(Func<string, bool> conditionEvaluation, 
+                                    Func<string, string[]> parametersExtraction)
+        {
+            this.conditionEvaluation = conditionEvaluation;
+            this.parametersExtraction = parametersExtraction;
+        }
+
+        public bool IsCommand(string commandLine)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
