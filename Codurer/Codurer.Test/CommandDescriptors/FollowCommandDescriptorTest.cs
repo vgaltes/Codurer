5e3b2192fe47ca272b53e1c181e0e9800522acda
vigarcia <vigarcia@plainconcepts.com>
2014-02-16 18:27:02 +0100

    Refactor

--- Codurer/Codurer.Test/CommandDescriptors/FollowCommandDescriptorTest.cs
+++ Codurer/Codurer.Test/CommandDescriptors/FollowCommandDescriptorTest.cs
@@ -1,39 +1,33 @@
 ﻿namespace CodurerApp.Test.CommandDescriptors
 {
-    using System;
-    using System.Linq;
     using CodurerApp.Commands;
     using CodurerApp.Services;
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Moq;
 
-    //[TestClass]
-    //public class FollowCommandDescriptorTest
-    //{
-    //    CommandDescriptor followCommandDescriptor = new CommandDescriptor(typeof(FollowCommand),
-    //            commandLine => commandLine.Contains("follows"),
-    //            commandLine => commandLine
-    //                .Split(new string[] { "follows" }, StringSplitOptions.RemoveEmptyEntries)
-    //                .Select(parameter => parameter.Trim())
-    //                .ToArray<string>());
+    [TestClass]
+    public class FollowCommandDescriptorTest
+    {
+        CommandDescriptor followCommandDescriptor =
+            (CommandDescriptor) new FollowCommandDescriptor();
 
-    //    string commandLine = "Alice follows Bob";
+        string commandLine = "Alice follows Bob";
 
-    //    [TestMethod]
-    //    public void WhenFollowing_EvaluatingTheCommandReturnsTrue()
-    //    {
-    //        var isFollowCommand = followCommandDescriptor.IsCommand(commandLine);
-    //        isFollowCommand.Should().BeTrue();
-    //    }
+        [TestMethod]
+        public void WhenFollowing_EvaluatingTheCommandReturnsTrue()
+        {
+            var isFollowCommand = followCommandDescriptor.CanHandle(commandLine);
+            isFollowCommand.Should().BeTrue();
+        }
 
-    //    [TestMethod]
-    //    public void WhenFollowing_ReturnsNewFollowCommand()
-    //    {
-    //        var codurer = new Codurer(new Mock<UserService>().Object);
-    //        var postCommand = followCommandDescriptor.GetCommand(codurer, commandLine);
+        [TestMethod]
+        public void WhenFollowing_ReturnsNewFollowCommand()
+        {
+            var codurer = new Codurer(new Mock<UserService>().Object);
+            var postCommand = followCommandDescriptor.GetCommand(codurer, commandLine);
 
-    //        postCommand.Should().BeOfType<FollowCommand>();
-    //    }
-    //}
+            postCommand.Should().BeOfType<FollowCommand>();
+        }
+    }
 }
