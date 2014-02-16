3c736ac6152cf4632c466caceeec44bf7a5a0c98
vigarcia <vigarcia@plainconcepts.com>
2014-02-16 19:12:11 +0100

    Refactor

---
+++ Codurer/CodurerApp/CommandDescriptors/CommandDescriptor.cs
@@ -0,0 +1,11 @@
+﻿namespace CodurerApp.CommandDescriptors
+{
+    using CodurerApp.Commands;
+
+    public interface CommandDescriptor
+    {
+        bool CanHandle(string commandLine);
+
+        Command GetCommand(Codurer codurer, string commandLine);
+    }
+}
